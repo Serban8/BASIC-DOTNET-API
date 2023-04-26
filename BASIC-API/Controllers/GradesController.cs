29c40e7a4cc163fd4cc41f06dde314064f2025c3
Serban8 <mihaiserban1234@gmail.com>
2023-04-26 16:55:53 +0300

    Add project files.

---
+++ BASIC-API/Controllers/GradesController.cs
@@ -0,0 +1,17 @@
+﻿using Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BASIC_API.Controllers
+{
+    [ApiController]
+    [Route("api/grades")]
+    public class GradesController : ControllerBase
+    {
+        private readonly GradeService gradeService;
+
+        public GradesController(GradeService gradeService)
+        {
+            this.gradeService = gradeService;
+        }
+    }
+}
---
+++ DataLayer/Repositories/GradesRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repositories
+{
+    public class GradesRepository : RepositoryBase<Class>
+    {
+        public GradesRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
